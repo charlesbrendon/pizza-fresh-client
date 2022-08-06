933f72acf7df349a61d9b047aad8eccb982da263
Charles Brendon <charlesbrendonss@gmail.com>
2022-08-06 02:49:06 -0300

    feat(boxLogin) create

--- src/assets/pages/Login/index.tsx
+++ src/assets/pages/Login/index.tsx
@@ -1,11 +1,14 @@
+import BoxLogin from "../../components/Menu/BoxLogin";
 import * as S from "./style";
 
 const Login = () => {
-    return (
-        <S.Login>
-			<S.LoginContent></S.LoginContent>
+	return (
+		<S.Login>
+			<S.LoginContent>
+				<BoxLogin />
+			</S.LoginContent>
 		</S.Login>
-    );
-}
+	);
+};
 
 export default Login;
