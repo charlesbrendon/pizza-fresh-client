f7663766ff7945f704760025b30254b17840036c
Charles Brendon <charlesbrendonss@gmail.com>
2022-08-06 02:42:38 -0300

    create login route

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { RoutePath } from 'types/routes';
 import Home from "./assets/pages/Home";
+import Login from "./assets/pages/Login";
 
 const Router = () => {
     return (
         <Routes>
+            <Route path={RoutePath.LOGIN} element={<Login />} />
             <Route path={RoutePath.HOME} element={<Home />} />
         </Routes>
     );
