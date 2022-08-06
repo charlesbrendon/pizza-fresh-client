fd8a41067180b9220f70e9d78500b595f406d85d
Charles Brendon <charlesbrendonss@gmail.com>
2022-08-06 05:33:48 -0300

    building ManageTables

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,5 @@
 import ManageProducts from "assets/components/ManageProducts";
+import ManageTables from "assets/components/ManageTables";
 import ManageUsers from "assets/components/ManageUsers";
 import Settings from "assets/pages/Settings";
 import { Route, Routes } from "react-router-dom";
@@ -7,19 +8,20 @@ import Home from "./assets/pages/Home";
 import Login from "./assets/pages/Login";
 
 const Router = () => {
-  return (
-    <Routes>
-      <Route path={RoutePath.LOGIN} element={<Login />} />
-      <Route path={RoutePath.HOME} element={<Home />} />
-      <Route path={RoutePath.SETTINGS} element={<Settings />}>
-        <Route
-          path={RoutePath.SETTINGS_PRODUCTS}
-          element={<ManageProducts />}
-        />
-        <Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
-      </Route>
-    </Routes>
-  );
-};
-
-export default Router;
+    return (
+      <Routes>
+        <Route path={RoutePath.LOGIN} element={<Login />} />
+        <Route path={RoutePath.HOME} element={<Home />} />
+        <Route path={RoutePath.SETTINGS} element={<Settings />}>
+          <Route path={RoutePath.SETTINGS_TABLES} element={<ManageTables />} />
+          <Route
+            path={RoutePath.SETTINGS_PRODUCTS}
+            element={<ManageProducts />}
+          />
+          <Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
+        </Route>
+      </Routes>
+    );
+  };
+  
+  export default Router;
