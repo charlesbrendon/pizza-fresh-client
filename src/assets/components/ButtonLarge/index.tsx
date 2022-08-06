7063670298f56694c365b54430f3857e1955f373
Charles Brendon <charlesbrendonss@gmail.com>
2022-08-06 03:07:17 -0300

    create button login route

---
+++ src/assets/components/ButtonLarge/index.tsx
@@ -0,0 +1,16 @@
+import * as S from "./style";
+import React, { ButtonHTMLAttributes } from "react";
+
+type ButtonType = ButtonHTMLAttributes<HTMLButtonElement>;
+
+type ButtonLargeProps = {
+    value: string;
+  } & ButtonType;
+
+const ButtonLarge = ({ value, ...props }: ButtonLargeProps) => {
+  return (
+    <S.ButtonLarge>Entrar</S.ButtonLarge>
+  );
+}
+
+export default ButtonLarge;
