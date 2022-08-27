73301c7cd1e2549cc4f8f60ca13b42c9269f50fb
Charles Brendon <charlesbrendonss@gmail.com>
2022-08-26 21:49:24 -0300

    implement AuthenticatedRoutes

--- src/router.tsx
+++ src/router.tsx
@@ -2,15 +2,24 @@ import ManageProducts from "assets/components/ManageProducts";
 import ManageTables from "assets/components/ManageTables";
 import ManageUsers from "assets/components/ManageUsers";
 import Settings from "assets/pages/Settings";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 import { RoutePath } from "types/routes";
 import Home from "assets/pages/Home";
 import Login from "assets/pages/Login";
+import { Auth } from "assets/helpers/Auth";
+
+const AuthenticatedRoutes = () => {
+  const isAuthenticated = Auth.isAuth();
+  return isAuthenticated ? <Outlet /> : <Navigate to={RoutePath.LOGIN} />;
+};
+
+
 
 const Router = () => {
-    return (
-      <Routes>
-        <Route path={RoutePath.LOGIN} element={<Login />} />
+  return (
+    <Routes>
+      <Route path={RoutePath.LOGIN} element={<Login />} />
+      <Route path="/" element={<AuthenticatedRoutes />}>
         <Route path={RoutePath.HOME} element={<Home />} />
         <Route path={RoutePath.SETTINGS} element={<Settings />}>
           <Route path={RoutePath.SETTINGS_TABLES} element={<ManageTables />} />
@@ -20,8 +29,9 @@ const Router = () => {
           />
           <Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
         </Route>
-      </Routes>
-    );
-  };
-  
-  export default Router;
+      </Route>
+    </Routes>
+  );
+};
+
+export default Router;
