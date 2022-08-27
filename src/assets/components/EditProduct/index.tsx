49a49e9c7830dfb405d6dc2cfa6aedf207b5067f
Charles Brendon <charlesbrendonss@gmail.com>
2022-08-26 22:44:15 -0300

    implement manage products

--- src/assets/components/EditProduct/index.tsx
+++ src/assets/components/EditProduct/index.tsx
@@ -1,43 +1,37 @@
 import { ReactComponent as Pencil } from "assets/icons/edit.svg";
+import { ProductResponse } from "types/api/product";
 import * as S from "./style";
 
-
-const EditProduct = () => {
+interface EditProductProps {
+    product: ProductResponse;
+  }
+  
+  const EditProduct = ({ product }: EditProductProps) => {
     return (
-        <S.EditProduct>
-            <>
-                <S.EditProductImage src="" alt="Pizza de..." />
-                <S.EditProductDetails>
-                    <S.EditProductDetailsName>Nome do Produto</S.EditProductDetailsName>
-                    <S.EditProductDetailsPrice>R$ 00,00</S.EditProductDetailsPrice>
-                    <S.EditProductDetailsDescription>Descrição do Produto</S.EditProductDetailsDescription>
-                </S.EditProductDetails>
-
-                <S.EditProductAction>
-                    <Pencil /> Editar
-                </S.EditProductAction>
-            </>
-            <S.EditFormGroup>
-                <S.EditForm
-                    type="text"
-                    placeholder="Título"
-                />
-                <S.EditForm
-                    type="number"
-                    placeholder="Preço"
-                />
-                <S.EditForm
-                    type="text"
-                    placeholder="Descrição"
-                />
-                <S.EditForm
-                    type="url"
-                    placeholder="Imagem"
-                />
-                <S.Delete>Deletar</S.Delete>
-            </S.EditFormGroup>
-        </S.EditProduct>
+      <S.EditProduct>
+        <>
+          <S.EditProductImage src="" alt="Pizza de..." />
+          <S.EditProductDetails>
+            <S.EditProductDetailsName>Nome do Produto</S.EditProductDetailsName>
+            <S.EditProductDetailsPrice>R$ 00,00</S.EditProductDetailsPrice>
+            <S.EditProductDetailsDescription>
+              Descrição do Produto
+            </S.EditProductDetailsDescription>
+          </S.EditProductDetails>
+  
+          <S.EditProductAction>
+            <Pencil /> Editar
+          </S.EditProductAction>
+        </>
+        <S.EditFormGroup>
+          <S.EditForm type="text" placeholder="Título" />
+          <S.EditForm type="number" placeholder="Preço" />
+          <S.EditForm type="text" placeholder="Descrição" />
+          <S.EditForm type="url" placeholder="Imagem" />
+          <S.Delete>Deletar</S.Delete>
+        </S.EditFormGroup>
+      </S.EditProduct>
     );
-}
-
-export default EditProduct;
+  };
+  
+  export default EditProduct;
